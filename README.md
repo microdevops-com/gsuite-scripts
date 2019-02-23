795c0652167c5d4e5048f1168928bb871ee79f66
Eugene Antonov <jetman@sysadm.ws>
2019-02-23 08:10:43 +0200

    help changes, cp cmd added

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
 # About
-Command line python scripts for G Suite to automate specific operations with Drive, Gmail etc. Used to generate invoices and reports, save them as PDF, create Gmail drafts with PDFs and so on.
+**Work In Progress**
+
+Command line python scripts for G Suite to automate specific operations with Drive, Gmail etc.
+Used to generate invoices and reports, save them as PDF, create Gmail drafts with PDFs and so on.
+Contains only commands needed in CRM.
+
+Drive API v3 used.
 
 # Usage
 ## Commands
 Commands use [IDs of Drive files](https://developers.google.com/drive/api/v3/about-files#file_ids).
 
-- `drive.py --ls` - List files, optionaly in folder specified with `--cd ID`.
-- `drive.py --mkdir "Some Folder Name"` - Create folder inside other folder specified with `--cd ID`.
+```
+./drive.py --help
+usage: drive.py [-h] [--debug] [--cd CD] [--name NAME] [--ls] [--mkdir MKDIR]
+                [--cp CP]
+
+Script to automate specific operations with G Suite Drive.
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --debug        enable debug
+  --cd CD        operate in folder specified by google drive ID CD
+  --name NAME    give new file name NAME, if required by command
+  --ls           returns ID<space>name of files, optionally with --cd
+  --mkdir MKDIR  create folder named MKDIR, only if it does not exist yet,
+                 returns ID of created or found folder, use always with --cd
+  --cp CP        copy file referenced by google drive ID CP only if it does
+                 not exist yet, returns ID of created file if created, use
+                 always with --cd and --name
+```
 ## Required Projects and APIs
 ### Developers Project
 Projects are managed on [Developers Console](https://console.developers.google.com/).
--- drive.py
+++ drive.py
@@ -23,9 +23,11 @@ if __name__ == "__main__":
     # Set parser and parse args
     parser = argparse.ArgumentParser(description='Script to automate specific operations with G Suite Drive.')
     parser.add_argument("--debug", dest="debug", help="enable debug", action="store_true")
-    parser.add_argument("--cd", dest="cd", help="operate in folder specified by ID")
-    parser.add_argument("--ls", dest="ls", help="list files, optionaly with --cd", action="store_true")
-    parser.add_argument("--mkdir", dest="mkdir", help="create folder, use with --cd")
+    parser.add_argument("--cd", dest="cd", help="operate in folder specified by google drive ID CD")
+    parser.add_argument("--name", dest="name", help="give new file name NAME, if required by command")
+    parser.add_argument("--ls", dest="ls", help="returns ID<space>name of files, optionally with --cd", action="store_true")
+    parser.add_argument("--mkdir", dest="mkdir", help="create folder named MKDIR, only if it does not exist yet, returns ID of created or found folder, use always with --cd")
+    parser.add_argument("--cp", dest="cp", help="copy file referenced by google drive ID CP only if it does not exist yet, returns ID of created file if created, use always with --cd and --name")
     args = parser.parse_args()
 
     # Set logger and console debug
@@ -43,73 +45,120 @@ if __name__ == "__main__":
         os.chdir(WORK_DIR)
 
         # Check env vars and connects
-        if args.ls or args.mkdir is not None:
-            
-            if SA_SECRETS_FILE is None:
-                logger.error("Env var SA_SECRETS_FILE missing")
-                sys.exit(1)
+        if SA_SECRETS_FILE is None:
+            logger.error("Env var SA_SECRETS_FILE missing")
+            sys.exit(1)
         
-        if args.ls or args.mkdir is not None:
-            
-            credentials = service_account.Credentials.from_service_account_file(SA_SECRETS_FILE, scopes=SCOPES)
-            drive_service = build('drive', 'v3', credentials=credentials)
+        credentials = service_account.Credentials.from_service_account_file(SA_SECRETS_FILE, scopes=SCOPES)
+        drive_service = build('drive', 'v3', credentials=credentials)
 
         # Do tasks
         if args.ls:
             
-            try:
+            page_token = None
+            while True:
 
-                page_token = None
-                while True:
+                if args.cd is not None:
+                    q = "'{0}' in parents".format(args.cd)
+                else:
+                    q = ""
 
-                    if args.cd is not None:
-                        q = "'{0}' in parents".format(args.cd)
-                    else:
-                        q = ""
-
+                # Try to list files, suppress errors
+                try:
                     response = drive_service.files().list(pageSize=100, fields="nextPageToken, files(id, name)", pageToken=page_token, q=q).execute()
                     items = response.get('files', [])
-
-                    if not items:
-                        print('No files found')
-                        logger.info('No files found')
-
-                    else:
-                        for item in items:
-                            print('{0} {1}'.format(item['id'], item['name']))
-                            logger.info('ls: {0} {1}'.format(item['id'], item['name']))
-
                     page_token = response.get('nextPageToken', None)
+                except Exception as e:
+                    logger.error('Listing in folder ID {0} failed'.format(args.cd))
+                    logger.info("Caught exception on execution:")
+                    logger.info(e)
+                    sys.exit(1)
 
-                    if page_token is None:
-                        break
+                # Empty out list, log only
+                if not items:
+                    logger.info('no files found')
 
-            except Exception as e:
-                logger.error("Caught exception on execution:")
-                logger.exception(e)
-                sys.exit(1)
+                else:
+                    for item in items:
+                        print('{0} {1}'.format(item['id'], item['name']))
+                        logger.info('{0} {1}'.format(item['id'], item['name']))
+
+                if page_token is None:
+                    break
 
         if args.mkdir:
             
             if args.cd is None:
-                logger.error("--cd FOLDER_ID is required for --mkdir command")
+                logger.error("--cd ID is required for --mkdir command")
                 sys.exit(1)
 
             try:
 
-                body = {
-                    'name': args.mkdir,
-                    'mimeType': "application/vnd.google-apps.folder",
-                    'parents': [args.cd]
-                }
+                # Query if the same file already exists
+                q = "'{0}' in parents and name = '{1}'".format(args.cd, args.mkdir)
 
-                folder = drive_service.files().create(body = body).execute()
-                print('Folder ID: {0}'.format(folder['id']))
-                logger.info('Folder ID: {0}'.format(folder['id']))
+                # Get only one page with 1 result
+                response = drive_service.files().list(pageSize=1, fields="files(id, name)", q=q).execute()
+                items = response.get('files', [])
+
+                if not items:
+
+                    body = {
+                        'name': args.mkdir,
+                        'mimeType': "application/vnd.google-apps.folder",
+                        'parents': [args.cd]
+                    }
+
+                    folder = drive_service.files().create(body = body).execute()
+                    print('{0}'.format(folder['id']))
+                    logger.info('{0}'.format(folder['id']))
+
+                else:
+                    
+                    # Print already found file ID
+                    print('{0}'.format(items[0]['id']))
+                    logger.info('{0}'.format(items[0]['id']))
 
             except Exception as e:
-                logger.error("Caught exception on execution:")
-                logger.exception(e)
+                logger.error('Creating {0} in folder ID {1} failed'.format(args.mkdir, args.cd))
+                logger.info("Caught exception on execution:")
+                logger.info(e)
+                sys.exit(1)
+
+        if args.cp:
+            
+            if args.cd is None:
+                logger.error("--cd ID is required for --cp command")
+                sys.exit(1)
+            
+            if args.name is None:
+                logger.error("--name NAME is required for --cp command")
+                sys.exit(1)
+
+            try:
+
+                # Query if the same file already exists
+                q = "'{0}' in parents and name = '{1}'".format(args.cd, args.name)
+
+                # Get only one page with 1 result
+                response = drive_service.files().list(pageSize=1, fields="files(id, name)", q=q).execute()
+                items = response.get('files', [])
+
+                if not items:
+
+                    body = {
+                        'name': args.name,
+                        'parents': [args.cd]
+                    }
+
+                    new_file = drive_service.files().copy(fileId=args.cp, body = body).execute()
+                    print('{0}'.format(new_file['id']))
+                    logger.info('{0}'.format(new_file['id']))
+
+            except Exception as e:
+                logger.error('Copying of {0} with name {1} in folder ID {2} failed'.format(args.cp, args.name, args.cd))
+                logger.info("Caught exception on execution:")
+                logger.info(e)
                 sys.exit(1)
 
     # Reroute catched exception to log
